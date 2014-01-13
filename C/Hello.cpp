6ea97be213c20aa8d7d540bdf89537000d56ffae
Kristoffer Larson <larson.kristoffert@gmail.com>
2014-01-13 05:22:33 -0800

    Create Hello.cpp

---
+++ C/Hello.cpp
@@ -0,0 +1,14 @@
+/*
+
+Programmers: Kris Larson
+
+Description: C++ compiler test program
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World! It took forever to get the compiler working...";
+	return 0;
+}
