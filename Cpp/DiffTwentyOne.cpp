bbc04b3296a6e6845d0a986e7b2d0a3623d62125
Kristoffer Larson <larson.kristoffert@gmail.com>
2014-01-15 13:29:43 -0800

    Create DiffTwentyOne.cpp

---
+++ Cpp/DiffTwentyOne.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int diffTwentyOne(int n);
+
+int main() {
+   int n = 22;
+	int num = diffTwentyOne(n);
+   printf("The abs difference between 21 and %d is %d", n, num);
+	return 0;
+}
+
+int diffTwentyOne(int n) {
+   return abs(21 - n);
+}
