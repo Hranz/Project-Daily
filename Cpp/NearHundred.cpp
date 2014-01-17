2797896cbc6c045eca4dbc524ea221555b911dc1
Kristoffer Larson <larson.kristoffert@gmail.com>
2014-01-16 23:18:47 -0800

    Create NearHundred.cpp

---
+++ Cpp/NearHundred.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+bool nearHundred(int n);
+
+int main() {
+   bool go = true;
+   int n;
+   cout << "Please enter an integer: ";
+   cin >> n;
+   
+   if (nearHundred(n))
+      printf("%d is within 10 of 100 or 200", n);
+   else
+      printf("%d isn't within 10 of 100 or 200", n);
+	return 0;
+}
+
+bool nearHundred(int n) {
+   if (abs(n) >= 90 && abs(n) <= 110)
+      return true;
+   else if (abs(n) >= 190 && abs(n) <= 210)
+      return true;
+   else
+      return false;
+}
