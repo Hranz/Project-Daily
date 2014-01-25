5ae9f41eaf2b2bdbebbe5ba7e7aea74b9e2b22e8
Kristoffer Larson <larson.kristoffert@gmail.com>
2014-01-25 14:15:48 -0800

    Create Hello.f90

---
+++ Fortran90/Hello.f90
@@ -0,0 +1,9 @@
+! Programmers: Kristoffer Larson
+! Date: January 25, 2014
+! 
+! Description: Trying to get minGW to compile
+
+ 
+program Hello
+  write (*,*) 'Hah! It finally compiled!'
+end program Hello
