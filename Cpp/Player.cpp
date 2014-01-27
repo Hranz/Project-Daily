6c4a0143174beb0618d15696e805bec59d34faa6
Kristoffer Larson <larson.kristoffert@gmail.com>
2014-01-27 11:02:46 -0800

    Update Player.cpp

--- Cpp/Player.cpp
+++ Cpp/Player.cpp
@@ -77,32 +77,3 @@ int main() {
    
    return 0;
 }
-
-
-
-
-// string FirstReverse(string str) { 
-//   char temp;
-//   int i = 0;
-//   int j = str.size() - 1;
-//   while (i < j) {
-//     temp = str[i];
-//     str[i] = str[j];
-//     str[j] = temp;
-//     i++;
-//     j--;
-//   }
-//   // code goes here   
-//   return str; 
-//             
-// }
-// 
-// int main() { 
-//   string str;
-//   cout << "Enter a string to be reversed: ";
-//   cin >> str;
-//   cout << FirstReverse(str);
-//   return 0;    
-// } 
-// 
-// 
