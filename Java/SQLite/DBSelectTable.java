a75a1c9a68609c14c3883857e96ccba88f61e6cc
Kristoffer Larson <larson.kristoffert@gmail.com>
2014-04-25 20:40:50 -0700

    Create DBSelectTable.java

---
+++ Java/SQLite/DBSelectTable.java
@@ -0,0 +1,70 @@
+/*Programmer: Kristoffer Larson
+ *Date: April 25, 2014
+ *
+ *Description: Connect to a DB and retrieve a table.
+ *
+ */
+
+import java.sql.*;
+
+public class DBSelectTable
+{
+   Connection c = null;
+   Statement stmt = null;
+   
+   public DBSelectTable()
+   {
+      //Connection c = null;
+      // try 
+//       {
+//          Class.forName("org.sqlite.JDBC");
+//          c = DriverManager.getConnection("jdbc:sqlite:test.db");
+//       } 
+//       catch ( Exception e ) 
+//       {
+//          System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+//          System.exit(0);
+//       }
+      //System.out.println("Opened database successfully");
+      select();
+   }
+   
+   public void select()
+   {
+      try {
+      Class.forName("org.sqlite.JDBC");
+      c = DriverManager.getConnection("jdbc:sqlite:test.db");
+      c.setAutoCommit(false);
+      System.out.println("Opened database successfully");
+
+      stmt = c.createStatement();
+      ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
+      while ( rs.next() ) {
+         int id = rs.getInt("id");
+         String  name = rs.getString("name");
+         int age  = rs.getInt("age");
+         String  address = rs.getString("address");
+         float salary = rs.getFloat("salary");
+         System.out.println( "ID = " + id );
+         System.out.println( "NAME = " + name );
+         System.out.println( "AGE = " + age );
+         System.out.println( "ADDRESS = " + address );
+         System.out.println( "SALARY = " + salary );
+         System.out.println();
+      }
+      rs.close();
+      stmt.close();
+      c.close();
+    } catch ( Exception e ) {
+      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+      System.exit(0);
+    }
+    System.out.println("Operation done successfully");
+   }
+   
+   public static void main(String[] args)
+   {
+      DBSelectTable app = new DBSelectTable();
+   }
+
+}
